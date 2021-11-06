5f22ce7426a979f9f53f4c00db97db2b2c26b88c
MEDFormat <81439134+MEDFormat@users.noreply.github.com>
2021-11-06 12:18:10 -0600

    Update mklibs.bat

--- mklibs.bat
+++ mklibs.bat
@@ -11,9 +11,6 @@ cl /c /I%LIBINC% /I%TGTINC% /Gd /GL /EHsc /nologo /O2 /D NDEBUG /D _CONSOLE /D _
 cl /c /I%LIBINC% /I%TGTINC% /Gd /GL /EHsc /nologo /O2 /D NDEBUG /D _CONSOLE /D _UNICODE /D UNICODE %LIBSRC%\medrec_m10.c
 lib /LTCG /nologo %LIBOBJ%\medlib_m10.obj %LIBOBJ%\medrec_m10.obj /OUT:%LIBOBJ%\libmed_m10%LIBSFX%.lib
 
-cl /c /I%LIBINC% /I%TGTINC% /Gd /GL /EHsc /nologo /O2 /D NDEBUG /D _CONSOLE /D _UNICODE /D UNICODE %LIBSRC%\dhnlib_d10.c
-lib /LTCG /nologo %LIBOBJ%\dhnlib_d10.obj /OUT:%LIBOBJ%\libdhn_d10%LIBSFX%.lib
-
 del %LIBOBJ%\*.obj
 
 
