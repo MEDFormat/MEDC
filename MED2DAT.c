bbe39a998d0e58c445655f373e348a4d8b2fbabc
MEDFormat <81439134+MEDFormat@users.noreply.github.com>
2022-08-19 08:10:14 -0600

    Add files via upload

--- MED2DAT.c
+++ MED2DAT.c
@@ -5,19 +5,20 @@
 
 
 #include "medlib_m11.h"
-#include "dhnlib_d11.h"
+
 
 #ifndef MAX_CHANNELS
-#define MAX_CHANNELS	512
+	#define MAX_CHANNELS	512
 #endif
-#define UV_DAT_CONVERSION_FACTOR		((sf8) 5.128205316306578)  // µV to DAT native amplitude
-
+#define UV_DAT_CONVERSION_FACTOR	((sf8) 5.128205316306578)  // µV to DAT native amplitude
+#define LS_MED2DAT_PROD_VER_MAJOR	((ui1) 1)
+#define LS_MED2DAT_PROD_VER_MINOR	((ui1) 0)
 
 
 int main(int argc, char *argv[])
 {
 	extern GLOBALS_m11		*globals_m11;
-	si1				out_dir[FULL_FILE_NAME_BYTES_m11], out_file[FULL_FILE_NAME_BYTES_m11], password[MAX_PASSWORD_STRING_BYTES_m11];
+	si1				out_dir[FULL_FILE_NAME_BYTES_m11], out_file[FULL_FILE_NAME_BYTES_m11], *password;
 	si2				*out_arr;
 	si4				n_channels, seg_offset, list_len;
 	ui8				flags;
@@ -35,13 +36,15 @@ int main(int argc, char *argv[])
 	// NOTE: in MacOS & Linux change resource limits before calling any functions that use system resources (e.g. printf())
 	adjust_open_file_limit_m11(MAX_OPEN_FILES_m11(MAX_CHANNELS, 1), TRUE_m11);
 
-	// usage
-	if (argc < 2 || argc > 6) {
-		fprintf(stderr, "\n\tUSAGE: %s MED_session_directory (or channel set with regex) [output_directory] [start_samp_num] [end_samp_num] [password]\n\n", argv[0]);
-		return(-1);
-	}
-
+	// initialize library
 	initialize_medlib_m11(FALSE_m11, FALSE_m11);
+	
+	// usage
+	if (argc < 2 || argc > 9) {
+		extract_path_parts_m11(argv[0], NULL, out_dir, NULL);
+		printf_m11("%c\n\t%s version %hhu.%hhu\n\n\tUSAGE: %s MED_directory (multiple w/ regex) [output_directory]  [start_time] [end_time] [start_samp_num] [end_samp_num] [password] [samp_num_ref_chan]\n\n", 7, out_dir, LS_MED2DAT_PROD_VER_MAJOR, LS_MED2DAT_PROD_VER_MINOR, argv[0]);
+		exit_m11(0);
+	}
 
 	// initialize time slice
 	initialize_time_slice_m11(&slice);
@@ -56,38 +59,60 @@ int main(int argc, char *argv[])
 	else
 		getcwd_m11(out_dir, FULL_FILE_NAME_BYTES_m11);
 
-	// start sample number
+	// start time
 	if (argc >= 4) {
 		if (*argv[3]) {  // LINUX strtol() returns zero for a zero-length string and does not set errno for EINVAL
 			if ((strcmp(argv[3], "start")) == 0)
-				slice.start_sample_number = BEGINNING_OF_SAMPLE_NUMBERS_m11;
+				slice.start_time = BEGINNING_OF_TIME_m11;
+			else
+				slice.start_time = (si8) strtol(argv[3], NULL, 10);
+		}
+	}
+
+	// end time
+	if (argc >= 5) {
+		if (*argv[4]) {  // LINUX strtol() returns zero for a zero-length string and does not set errno for EINVAL
+			if ((strcmp(argv[4], "end")) == 0)
+				slice.end_time = END_OF_TIME_m11;
 			else {
-				slice.start_sample_number = (si8) strtol(argv[3], NULL, 10);
-				if (slice.start_sample_number)
-					--slice.start_sample_number;  // convert to c indexing
+				slice.end_time = (si8) strtol(argv[4], NULL, 10);
+				if (slice.end_time == 0)
+					slice.end_time = UUTC_NO_ENTRY_m11;
 			}
 		}
 	}
 
+	// start sample number
+	if (argc >= 6) {
+		if (*argv[5]) {  // LINUX strtol() returns zero for a zero-length string and does not set errno for EINVAL
+			if ((strcmp(argv[5], "start")) == 0)
+				slice.start_sample_number = BEGINNING_OF_SAMPLE_NUMBERS_m11;
+			else
+				slice.start_sample_number = (si8) strtol(argv[5], NULL, 10);
+		}
+	}
+
 	// end sample number
-	if (argc >= 5) {
-		if (*argv[4]) {  // LINUX strtol() returns zero for a zero-length string and does not set errno for EINVAL
-			if ((strcmp(argv[4], "end")) == 0)
+	if (argc >= 7) {
+		if (*argv[6]) {  // LINUX strtol() returns zero for a zero-length string and does not set errno for EINVAL
+			if ((strcmp(argv[6], "end")) == 0)
 				slice.end_sample_number = END_OF_SAMPLE_NUMBERS_m11;
 			else {
-				slice.end_sample_number = (si8) strtol(argv[4], NULL, 10);
+				slice.end_sample_number = (si8) strtol(argv[6], NULL, 10);
 				if (slice.end_sample_number == 0)
 					slice.end_time = SAMPLE_NUMBER_NO_ENTRY_m11;
-				else
-					--slice.end_sample_number;  // convert to c indexing
 			}
 		}
 	}
 
 	// password
-	*password = 0;
-	if (argc == 6)
-		strcpy(password, argv[5]);
+	password = NULL;
+	if (argc >= 8)
+		password = argv[7];
+
+	// sample number reference channel name (base name only)
+	if (argc == 9)
+		extract_path_parts_m11(argv[8], NULL, globals_m11->reference_channel_name, NULL);
 
 	// read session
 	printf_m11("\nReading MED data ...  "); fflush(stdout);
@@ -95,6 +120,10 @@ int main(int argc, char *argv[])
 	sess = read_session_m11(NULL, &slice, file_list, list_len, flags, password);
 	if (sess == NULL) {
 		error_message_m11("%s(): error reading session\n", __FUNCTION__);
+		exit_m11(1);
+	}
+	if (sess->Sgmt_records[0].sampling_frequency == FREQUENCY_VARIABLE_m11) {
+		error_message_m11("%s(): MED2DAT does not currently handle variable sampling frequency sesssions\n", __FUNCTION__);
 		exit_m11(1);
 	}
 	slice = sess->time_slice;
@@ -123,10 +152,10 @@ int main(int argc, char *argv[])
 				seg = sess->time_series_channels[m]->segments[j];
 				cps = seg->time_series_data_fps->parameters.cps;
 				tmp_sf8 = (sf8) cps->decompressed_data[k] * UV_DAT_CONVERSION_FACTOR;
-				if (tmp_sf8 > (sf8) 32767.0)
-					tmp_sf8 = (sf8) 32767.0;
-				else if (tmp_sf8 < (sf8) -32768.0)
-					tmp_sf8 = (sf8) -32768.0;
+				if (tmp_sf8 > (sf8) POS_INF_SI2_m11)  // 0x7FFF
+					tmp_sf8 = (sf8) POS_INF_SI2_m11;
+				else if (tmp_sf8 < (sf8) NAN_SI2_m11)  // 0x8000
+					tmp_sf8 = (sf8) NAN_SI2_m11;
 				out_arr[m] = (si2) round(tmp_sf8);
 			}
 			++curr_samp;
@@ -146,7 +175,7 @@ int main(int argc, char *argv[])
 	
 	// clean up
 	free_session_m11(sess, TRUE_m11);
-	free_m11((void *) out_arr, __FUNCTION__, __LINE__);
+	free_m11((void *) out_arr, __FUNCTION__, __LINE__, USE_GLOBAL_BEHAVIOR_m11);
 	free_globals_m11(TRUE_m11);
 	
 	return(0);
