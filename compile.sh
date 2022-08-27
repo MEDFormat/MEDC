a8ea593ad12e98534332b18aaaae75774adb574c
MEDFormat <81439134+MEDFormat@users.noreply.github.com>
2022-08-27 13:10:59 -0600

    Add files via upload

--- compile.sh
+++ compile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 
+
 #
 # This script relies on the following directory hierarchy and library existing:
 # /Volumes/devdrv (or /mnt/devdrv) (mount point)
@@ -16,6 +17,7 @@
 #
 
 
+
 #########################
 #### Start Edit Here ####
 #########################
@@ -29,23 +31,28 @@ CC="clang"
 # OS: "MacOS" or "Linux"
 OS="MacOS"
 
-# targets_m11.h file location: "local" or "library"
+# targets_m11.h file: "local" or "library"
 TGT_FILE="library"
 
-#### Set DEVDRV here ####
-
-if [ $OS = "Linux" ]; then
-	DEVDRV="/mnt/dhndev"
-	LIBSFX="_lin"
-elif [  $OS = "MacOS" ]; then
-	DEVDRV="/Volumes/dhndev"
-	LIBSFX="_mac"
-fi
+# binary: "x86", "arm", or "all"
+BIN="all"
 
 ########################
 #### Stop Edit Here ####
 ########################
 
+
+if [ $OS = "Linux" ]; then
+	DEVDRV="/mnt/devdrv"
+	LIBSFX="_lin"
+elif [  $OS = "MacOS" ]; then
+	DEVDRV="/Volumes/devdrv";
+	LIBSFX="_mac"
+fi
+
+if [ $CC = "icc" ]; then
+	BIN="x86"
+fi
 
 PRGINC=${DEVDRV}/$PRG
 PRGSRC=$PRGINC
@@ -53,7 +60,7 @@ PRGOBJ=${PRGSRC}/$OS
 LIBINC=${DEVDRV}/lib
 LIBOBJ=${LIBINC}/$OS
 
-CC_OPT="-O2 -Wall -fms-extensions -Wno-microsoft-anon-tag"
+CC_OPT="-O3 -Wall -fms-extensions -Wno-microsoft-anon-tag"
 if [ $CC = "icc" ]; then
 	CC="/opt/intel/bin/icc"
 	CC_OPT="${CC_OPT} -Qoption,cpp,--extended_float_types -static-intel"
@@ -72,9 +79,39 @@ fi
 
 
 # compile
-CC_CMD="$CC -o ${PRGOBJ}/$PRG $CC_OPT -I$TGTINC -I$PRGINC -I$LIBINC ${PRGSRC}/${PRG}.c -L$LIBOBJ -lmed_m11$LIBSFX"
-echo " "
-echo $CC_CMD
-$CC_CMD
-echo " "
+if [ $BIN = "x86" ]; then
+	TMP_CC_OPT="${CC_OPT} -arch x86_64"
+	CC_CMD="$CC -o ${PRGOBJ}/$PRG -Wall $TMP_CC_OPT -I$TGTINC -I$PRGINC -I$LIBINC ${PRGSRC}/${PRG}.c -L$LIBOBJ -lmed_m11$LIBSFX"
+	echo " "
+	echo $CC_CMD
+	$CC_CMD
+elif [ $BIN = "arm" ]; then
+	TMP_CC_OPT="${CC_OPT} -target arm64-apple-macos12 -mmacosx-version-min=12.0"
+	CC_CMD="$CC -o ${PRGOBJ}/$PRG -Wall $TMP_CC_OPT -I$TGTINC -I$PRGINC -I$LIBINC ${PRGSRC}/${PRG}.c -L$LIBOBJ -lmed_m11$LIBSFX"
+	echo " "
+	echo $CC_CMD
+	$CC_CMD
+elif [ $BIN = "all" ]; then	
+	TMP_CC_OPT="${CC_OPT} -arch x86_64"
+	CC_CMD="$CC -o ${PRGOBJ}/${PRG}_x86 -Wall $TMP_CC_OPT -I$TGTINC -I$PRGINC -I$LIBINC ${PRGSRC}/${PRG}.c -L$LIBOBJ -lmed_m11$LIBSFX"
+	echo " "
+	echo $CC_CMD
+	$CC_CMD
+
+	TMP_CC_OPT="${CC_OPT} -target arm64-apple-macos12 -mmacosx-version-min=12.0"
+	CC_CMD="$CC -o ${PRGOBJ}/${PRG}_arm -Wall $TMP_CC_OPT -I$TGTINC -I$PRGINC -I$LIBINC ${PRGSRC}/${PRG}.c -L$LIBOBJ -lmed_m11$LIBSFX"
+	echo " "
+	echo $CC_CMD
+	$CC_CMD
+
+	echo " "
+	CMD="lipo -create -output ${PRGOBJ}/$PRG ${PRGOBJ}/${PRG}_x86 ${PRGOBJ}/${PRG}_arm"
+	echo $CMD; echo " "
+	$CMD
+
+	CMD="rm ${PRGOBJ}/${PRG}_x86 ${PRGOBJ}/${PRG}_arm"
+	echo $CMD; echo " "
+	$CMD
+fi
+
 
