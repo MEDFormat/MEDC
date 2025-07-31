7e4fc11fe50636012c7b4c469ce069741121e9e0
MEDFormat <81439134+MEDFormat@users.noreply.github.com>
2025-07-31 12:19:43 -0600

    Add files via upload

--- mklibs.bat
+++ mklibs.bat
@@ -1,5 +1,19 @@
 
 ::
+:: This script relies on the following directory hierarchy and library existing:
+:: DEVDRV: (drive)
+::	lib (dir)
+::		Windows (dir)
+::			targets_m13.h (file) (edited to define "WINDOWS_m13")
+::			libmed_m13_win.lib (file)
+::	MED2RAW (dir)
+::		Windows (dir)
+::			MED2RAW.c (file)
+::
+:: Link the libraries with "/link %LIBOBJ%\libmed_m13_%LIBSFX%.lib" in executables (variable defined below)
+::
+
+::
 :: For 64-bit native code When compiling with the Visual Studio compiler ("cl") from the command line:
 :: 	1) Use "x64 Native Tools Command Prompt for VS 2019" as the terminal program. (This just sets a number of necessary environment variables.)
 ::	2) Set VSCMD_ARG_HOST_ARCH & VSCMD_ARG_TGT_ARCH to x64, as below.
@@ -11,9 +25,9 @@
 set VSCMD_ARG_HOST_ARCH=x64
 set VSCMD_ARG_TGT_ARCH=x64
 
-set DHNDRV=Z
+set DEVDRV=Z
 
-set LIBSRC=%DHNDRV%:\lib
+set LIBSRC=%DEVDRV%:\lib
 set LIBINC=%LIBSRC%
 set LIBOBJ=%LIBSRC%\Windows
 set TGTINC=%LIBOBJ%
